511358d5d4c2c1c838548647d73c5705bae22af6
ONILLOJU <darkhurt351@gmail.com>
2025-11-04 14:45:10 +0100

    update index

---
+++ Index.php
@@ -0,0 +1,7 @@
+<?php
+
+// 2.1 : Charger la classe d'autoload
+require_once __DIR__ . '/Helpers/Psr4AutoloaderClass.php';
+
+// Créer l'instance du loader
+$loader = new \Helpers\Psr4AutoloaderClass();
