e25f9a8773531697b887e84eb84aeb24b6c53c35
ONILLOJU <darkhurt351@gmail.com>
2025-11-04 15:31:39 +0100

    update exo3

--- Index.php
+++ Index.php
@@ -1,7 +1,21 @@
 <?php
 
-// 2.1 : Charger la classe d'autoload
+// Requires
+use League\Plates\Engine;
 require_once __DIR__ . '/Helpers/Psr4AutoloaderClass.php';
+require_once __DIR__ . '/Vendor/Plates/src/Engine.php';
 
-// Créer l'instance du loader
+// Créer une instance et enregistrer l'autoloader
 $loader = new \Helpers\Psr4AutoloaderClass();
+$loader->register();
+
+// Enregistrer les namespace
+$loader->addNamespace('\Helpers', 'Helpers');
+$loader->addNamespace('\League\Plates', 'Vendor/Plates/src');
+
+// Instancier le moteur de templates
+$templates = new League\Plates\Engine(__DIR__ . '/Views');
+
+// Afficher la vue
+echo $templates->render('home', ['gameName' => 'Genshin Impact']);
+
---
+++ Views/Home.php
@@ -0,0 +1,4 @@
+<?php
+$this->layout('template', ['title' => 'TP Mihoyo']);
+?>
+<h1>Collection <?= $this->e($gameName) ?></h1>
