1eff2d0d19cfa641d2431b23b8bb4c9b9b3e0e8a
darkhurt70 <darkhurt351@gmail.com>
2025-11-05 16:26:38 +0100

    update Fin partie 2, ajout css

--- Views/Home.php
+++ Views/Home.php
@@ -1,11 +1,38 @@
 <?php
-$this->layout('template', ['title' => 'TP Mihoyo']);
-?>
+//$this->layout('template', ['title' => 'TP Mihoyo']);
+//?>
+<!--<h1>Collection --><?php //= $this->e($gameName) ?><!--</h1>-->
+<!---->
+<!--<h2>Affichage brut (debug)</h2>-->
+<!--<pre>-->
+<?php //var_dump($listPersonnage); ?>
+<?php //var_dump($first); ?>
+<?php //var_dump($other); ?>
+<!--</pre>-->
+
+<?php $this->layout('template', ['title' => 'TP Mihoyo']) ?>
+
 <h1>Collection <?= $this->e($gameName) ?></h1>
 
-<h2>Affichage brut (debug)</h2>
-<pre>
-<?php var_dump($listPersonnage); ?>
-<?php var_dump($first); ?>
-<?php var_dump($other); ?>
-</pre>
+<div class="container">
+    <div class="row">
+        <?php foreach ($listPersonnage as $perso): ?>
+            <div class="card" style="width: 200px; margin: 10px; border: 1px solid #ccc; border-radius: 10px; overflow: hidden;">
+                <img src="<?= $perso->getUrlImg(); ?>" alt="<?= $this->e($perso->getName()); ?>" style="width: 100%; height: auto;">
+                <div style="padding: 10px;">
+                    <h4><?= $this->e($perso->getName()); ?></h4>
+                    <p>Élément : <?= $this->e($perso->getElement()); ?></p>
+                    <p>Classe : <?= $this->e($perso->getUnitclass()); ?></p>
+                    <p>Origine : <?= $this->e($perso->getOrigin()); ?></p>
+                    <p>Rareté : <?= $this->e($perso->getRarity()); ?>★</p>
+
+                    <!-- Actions -->
+                    <div style="margin-top: 10px;">
+                        <a href="?route=edit&id=<?= $perso->getId(); ?>" title="Modifier">✏️</a>
+                        <a href="?route=delete&id=<?= $perso->getId(); ?>" title="Supprimer">🗑️</a>
+                    </div>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
+</div>
